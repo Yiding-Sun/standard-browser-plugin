5efdea2b752c9934ae3d695cb42742e1c19b9037
sunyiding <sunyidingman@foxmail.com>
2020-10-08 20:17:54 +0800

    Rename the project

--- js/data.js
+++ js/data.js
@@ -21,30 +21,30 @@ var listenTypes = {
 
 var baiduHomepagePatten = {
     maps: [
-        { name: "#sb_inputbox", to: "#kw", types: [listenTypes.text] },
-        { name: "#sb_searchbutton", to: "#su", types: [listenTypes.button] },
-        { name: "#s_lg_img", to: "#sb_img", types: [listenTypes.img] }
+        { name: "#sbp_inputbox", to: "#kw", types: [listenTypes.text] },
+        { name: "#sbp_searchbutton", to: "#su", types: [listenTypes.button] },
+        { name: "#s_lg_img", to: "#sbp_img", types: [listenTypes.img] }
     ]
 }
 var bingHomepagePatten = {
     maps: [
-        { name: "#sb_inputbox", to: "#sb_form_q", types: [listenTypes.text] },
-        { name: "#sb_searchbutton", to: "#sb_form_go", types: [listenTypes.button] },
-        { name: ".hp_sw_logo", to: "#sb_img", types: [listenTypes.classType] },
+        { name: "#sbp_inputbox", to: "#sb_form_q", types: [listenTypes.text] },
+        { name: "#sbp_searchbutton", to: "#sb_form_go", types: [listenTypes.button] },
+        { name: ".hp_sw_logo", to: "#sbp_img", types: [listenTypes.classType] },
     ]
 }
 var soHomepagePatten = {
     maps: [
-        { name: "#sb_inputbox", to: "#input", types: [listenTypes.text] },
-        { name: "#sb_searchbutton", to: "#search-button", types: [listenTypes.button] },
-        { name: ".skin-logo", to: "#sb_img", types: [listenTypes.classType] }
+        { name: "#sbp_inputbox", to: "#input", types: [listenTypes.text] },
+        { name: "#sbp_searchbutton", to: "#search-button", types: [listenTypes.button] },
+        { name: ".skin-logo", to: "#sbp_img", types: [listenTypes.classType] }
     ]
 }
 var sogouHomepagePatten = {
     maps: [
-        { name: "#sb_inputbox", to: "#query", types: [listenTypes.text] },
-        { name: "#sb_searchbutton", to: "#stb", types: [listenTypes.button] },
-        { name: ".content .logo span", to: "#sb_img", types: [css(["background", "width", "height"])] }
+        { name: "#sbp_inputbox", to: "#query", types: [listenTypes.text] },
+        { name: "#sbp_searchbutton", to: "#stb", types: [listenTypes.button] },
+        { name: ".content .logo span", to: "#sbp_img", types: [css(["background", "width", "height"])] }
     ]
 }
 
@@ -59,6 +59,6 @@ var data = [
             { url: /^https?\:\/\/www\.so\.com\/([^s][^?][\u0000-\u00ff]*)?$/, patten: soHomepagePatten },
             { url: /^https?\:\/\/www\.sogou\.com\/$/, patten: sogouHomepagePatten }
         ],
-        html: "<img id='sb_img'/><br/><input id='sb_inputbox'></input><br/><button id='sb_searchbutton'>Search!</button>"
+        html: "<img id='sbp_img'/><br/><input id='sbp_inputbox'></input><br/><button id='sbp_searchbutton'>Search!</button>"
     }
 ]
--- js/dist/data.dev.js
+++ js/dist/data.dev.js
@@ -28,61 +28,61 @@ var listenTypes = {
 };
 var baiduHomepagePatten = {
   maps: [{
-    name: "#sb_inputbox",
+    name: "#sbp_inputbox",
     to: "#kw",
     types: [listenTypes.text]
   }, {
-    name: "#sb_searchbutton",
+    name: "#sbp_searchbutton",
     to: "#su",
     types: [listenTypes.button]
   }, {
     name: "#s_lg_img",
-    to: "#sb_img",
+    to: "#sbp_img",
     types: [listenTypes.img]
   }]
 };
 var bingHomepagePatten = {
   maps: [{
-    name: "#sb_inputbox",
+    name: "#sbp_inputbox",
     to: "#sb_form_q",
     types: [listenTypes.text]
   }, {
-    name: "#sb_searchbutton",
+    name: "#sbp_searchbutton",
     to: "#sb_form_go",
     types: [listenTypes.button]
   }, {
     name: ".hp_sw_logo",
-    to: "#sb_img",
+    to: "#sbp_img",
     types: [listenTypes.classType]
   }]
 };
 var soHomepagePatten = {
   maps: [{
-    name: "#sb_inputbox",
+    name: "#sbp_inputbox",
     to: "#input",
     types: [listenTypes.text]
   }, {
-    name: "#sb_searchbutton",
+    name: "#sbp_searchbutton",
     to: "#search-button",
     types: [listenTypes.button]
   }, {
     name: ".skin-logo",
-    to: "#sb_img",
+    to: "#sbp_img",
     types: [listenTypes.classType]
   }]
 };
 var sogouHomepagePatten = {
   maps: [{
-    name: "#sb_inputbox",
+    name: "#sbp_inputbox",
     to: "#query",
     types: [listenTypes.text]
   }, {
-    name: "#sb_searchbutton",
+    name: "#sbp_searchbutton",
     to: "#stb",
     types: [listenTypes.button]
   }, {
     name: ".content .logo span",
-    to: "#sb_img",
+    to: "#sbp_img",
     types: [css(["background", "width", "height"])]
   }]
 };
@@ -101,5 +101,5 @@ var data = [{
     url: /^https?\:\/\/www\.sogou\.com\/$/,
     patten: sogouHomepagePatten
   }],
-  html: "<img id='sb_img'/><br/><input id='sb_inputbox'></input><br/><button id='sb_searchbutton'>Search!</button>"
+  html: "<img id='sbp_img'/><br/><input id='sbp_inputbox'></input><br/><button id='sbp_searchbutton'>Search!</button>"
 }];
